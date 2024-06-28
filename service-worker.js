3b3e462a0f997baede0edce13f962a5a3e36c9fb
Isaac Hernandez <isaacrdzcmd@gmail.com>
2024-06-28 12:35:02 -0600

    recibiendo imagenes y iconos

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,8 @@ self.addEventListener('push', function(event) {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
       body: data.body,
-      icon: 'icon.png'
+      icon: data.icon,
+      image: data.image
     });
   });
   
